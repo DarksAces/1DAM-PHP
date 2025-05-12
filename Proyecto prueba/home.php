ec0fefed817718d7b4be6be66fcf7262a275ee77
Daniel <danielgarciabrun@gmail.com>
2025-05-12 12:15:17 +0200

    Update

--- Proyecto prueba/home.php
+++ Proyecto prueba/home.php
@@ -1,5 +1,10 @@
+<?php
+$test = "¡PHP está funcionando!";
+?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +13,9 @@
 
 <body>
     <h1>Monlaueer</h1>
-    <?php
-        $test = "¡PHP está funcionando!";
-    ?>
-    <?php echo "<h1>$test</h1>"?>
-     
+
+    <?php echo "<h1>$test</h1>" ?>
+
 </body>
+
 </html>
